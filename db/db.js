89195e2794f9c1e6e5469862fcb786388e5e10d6
Zach Rizzo <RizzoZachary2@gmail.com>
2022-03-19 10:13:49 -0500

    barebones backend set up

--- db/db.js
+++ db/db.js
@@ -1,9 +1,28 @@
-const Sequelize = require('sequelize');
+const faker = require('faker');
+const { Sequelize, STRING } = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/dealers_choice_full_stack');
+
+const Color = sequelize.define('Color', {
+  name:{
+    type: STRING
+  }
+})
+
+Color.createRandom = () => {
+  return Color.create({name: faker.commerce.color()})
+}
 
 
 const syncAndSeed = async () => {
   await sequelize.sync({force: true});
+  
+  for(let i = 0; i < 10; i++){
+    await Color.createRandom();
+  }
+
 }
 
-syncAndSeed();
+module.exports = {
+  Color,
+  syncAndSeed
+}
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const { syncAndSeed, Color } = require('./db/db');
 
 app.use('/dist', express.static(path.join(__dirname, 'dist')));
 app.get('/', (req, res)=> res.sendFile(path.join(__dirname, 'index.html')));
@@ -8,3 +9,19 @@ app.get('/', (req, res)=> res.sendFile(path.join(__dirname, 'index.html')));
 const port = process.env.PORT || 3000;
 
 app.listen(port, ()=> console.log(`listening on port ${port}`));
+
+const init = async () => {
+  await syncAndSeed();
+}
+
+init();
+
+app.get('/colors', async (req, res, next) => {
+  try{
+    const colors = await Color.findAll()
+    res.send(colors);
+  }
+  catch(error){
+    next(error);
+  }
+})
