fcbcd14422e08987b5587a354f3dbc8448e5ae8d
Zach Rizzo <RizzoZachary2@gmail.com>
2022-03-19 10:25:24 -0500

    have main component connected to the store

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
+import {Provider, connect} from 'react-redux';
+import store from './store';
 
-render(<hr />, document.querySelector('#root'));
+class _App extends React.Component{
+  render(){
+    return(
+      <div>
+        <ul>
+          <hr/>
+          <hr/>
+        </ul>
+      </div>
+    )
+  }
+}
+
+const App = connect()(_App)
+
+ReactDOM.render(<Provider store={store}><App/></Provider>, document.querySelector('#root'));
